49e0ef35446328587e8b894d116f8c3743d131ce
Pedro Gois <pedromorais.gois@gmail.com>
2021-04-20 12:34:20 -0300

    alteração

---
+++ index.php
@@ -0,0 +1,14 @@
+  
+<?php
+
+	var_dump($_GET);
+
+	echo '<hr>';
+
+	echo '<a href="sobre">Sobre</a>';
+
+	if ($_GET) {
+		$url = explode('/', $_GET['url']);
+
+		require_once 'pages/'.$url[0].'.php';
+	}
